5eb25a6c9a88e8c10e92cffd6cea9037d416fd1e
N7_Remax <ditritusa@gmail.com>
2019-12-10 23:36:35 +0200

    created logo module and connect it to App

--- src/App/App.js
+++ src/App/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.scss';
 
 import '../common/style/style.scss'
+import Logo from './Logo/Logo';
 
 function App() {
   return (
     <div className="App">
       <div className="row">
-        
+        <div className="logo-place">
+          <Logo />
+        </div>
       </div>
     </div>
   );
