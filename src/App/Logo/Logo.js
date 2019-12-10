ecedb960ddf9bef05b88c6b088c66792c0e96675
N7_Remax <ditritusa@gmail.com>
2019-12-11 00:39:02 +0200

    added slogan to logo module

--- src/App/Logo/Logo.js
+++ src/App/Logo/Logo.js
@@ -3,11 +3,15 @@ import React from 'react';
 import './logo.scss';
 
 function Logo() {
-  return (
-    <div className="logo">
-        <a href="#"><h1>UALUCK</h1></a>
-    </div>
-  );
+    return (
+        <div className="logo">
+            <a href="#"><h1>UALUCK</h1></a>
+            <div className="slogan">
+                <p>Грай та вигравай</p>
+                <p className="second-p">Найнадійніший букмекер</p>
+            </div>
+        </div>
+    );
 }
 
 export default Logo;
