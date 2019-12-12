86f9aad3c30df35df8b93adef294db6fa39c9429
N7_Remax <ditritusa@gmail.com>
2019-12-12 23:30:56 +0200

    added image, added flex to main module

--- src/App/Main/Main.js
+++ src/App/Main/Main.js
@@ -5,16 +5,21 @@ import './main.scss';
 function Main() {
     return (
         <main className="main">
-            <div className="action">
-                <p>Бонус до <span>10 000 &#8372;</span></p>
-                <p>всім клієнтам</p>
-            </div>
             <div className="registration-block">
-                <div className="buttons-place">
-                    <div className="button"><span>в 1 клік</span></div>
-                    <div className="button"><span>По Email</span></div>
-                    <div className="button"><span>Соц. мережі</span></div>
+                <div className="action">
+                    <p>Бонус до <span>10 000 &#8372;</span></p>
+                    <p>всім клієнтам</p>
                 </div>
+                <div className="registration-buttons">
+                    <div className="buttons-place">
+                        <div className="button"><span>в 1 клік</span></div>
+                        <div className="button"><span>По Email</span></div>
+                        <div className="button"><span>Соц. мережі</span></div>
+                    </div>
+                </div>
+            </div>
+            <div className="img-place">
+                <img src={require('../../common/images/soccer-player.png')} alt="soccer player"></img>
             </div>
         </main>
     );
