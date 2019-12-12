d20fc3eb52a48ac2ccd2981781ad8b7ac33f5e42
N7_Remax <ditritusa@gmail.com>
2019-12-12 22:50:24 +0200

    added buttons layout

--- src/App/Main/Main.js
+++ src/App/Main/Main.js
@@ -9,6 +9,13 @@ function Main() {
                 <p>Бонус до <span>10 000 &#8372;</span></p>
                 <p>всім клієнтам</p>
             </div>
+            <div className="registration-block">
+                <div className="buttons-place">
+                    <div className="button"><span>в 1 клік</span></div>
+                    <div className="button"><span>По Email</span></div>
+                    <div className="button"><span>Соц. мережі</span></div>
+                </div>
+            </div>
         </main>
     );
 }
