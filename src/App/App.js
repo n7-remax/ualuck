8f46a8962ed865cfacf6712a77b8bace4946368a
N7_Remax <ditritusa@gmail.com>
2019-12-16 00:15:52 +0200

    created footer module and connect it to App

--- src/App/App.js
+++ src/App/App.js
@@ -4,6 +4,7 @@ import './App.scss';
 import '../common/style/style.scss'
 import Logo from './Logo/Logo';
 import Main from './Main/Main';
+import Footer from './Footer/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div className="row">
           <Logo />
           <Main />
+          <Footer />
       </div>
     </div>
   );
