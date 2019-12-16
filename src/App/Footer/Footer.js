25738c5233decf7dcbaba40de110e3e7e928194c
N7_Remax <ditritusa@gmail.com>
2019-12-17 00:41:59 +0200

    removed unused class in footer

--- src/App/Footer/Footer.js
+++ src/App/Footer/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
         <footer className="footer">
             <div className="quality">
                 <div className="standard-mark"></div>
-                <div className="test">
+                <div>
                     <span className="verified">Перевірено</span>
                     <span>«рейтингом букмекерів»</span>
                 </div>
