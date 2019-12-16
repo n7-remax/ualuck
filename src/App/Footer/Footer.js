f3c10b5c8817d8993d06ce3641613209ed13c2ab
N7_Remax <ditritusa@gmail.com>
2019-12-16 23:16:56 +0200

    added quality mark and text to footer

--- src/App/Footer/Footer.js
+++ src/App/Footer/Footer.js
@@ -5,6 +5,13 @@ import './footer.scss';
 function Footer() {
     return (
         <footer className="footer">
+            <div className="quality">
+                <div className="standard-mark"></div>
+                <div className="test">
+                    <span className="verified">Перевірено</span>
+                    <span>«рейтингом букмекерів»</span>
+                </div>
+            </div>
         </footer>
     );
 }
