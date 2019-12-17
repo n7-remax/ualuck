a5f2656f499115ad17f0a4912c062dbb7e0c42e0
N7_Remax <ditritusa@gmail.com>
2019-12-17 19:28:39 +0200

    update logo style

--- src/App/Logo/Logo.js
+++ src/App/Logo/Logo.js
@@ -5,7 +5,7 @@ import './logo.scss';
 function Logo() {
     return (
         <div className="logo">
-            <a href="#"><h1>UALUCK</h1></a>
+            <a href="#"><h1><span className="yellow">U</span><span className="blue">A</span>LUCK</h1></a>
             <div className="slogan">
                 <p>Грай та вигравай</p>
                 <p className="second-p">Найнадійніший букмекер</p>
