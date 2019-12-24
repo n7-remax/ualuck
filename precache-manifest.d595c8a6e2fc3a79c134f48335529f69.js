c7d02a69e6e504b4798590a8dec1c8cf1dd8a096
N7_Remax <ditritusa@gmail.com>
2019-12-25 00:56:04 +0200

    Updates

---
+++ precache-manifest.d595c8a6e2fc3a79c134f48335529f69.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f51cf93cafd6cfe721a1a87f8633644f",
+    "url": "/ualuck/index.html"
+  },
+  {
+    "revision": "72246167e03a2d21680e",
+    "url": "/ualuck/static/css/main.ffd27f39.chunk.css"
+  },
+  {
+    "revision": "2b332440f6c1020ba87a",
+    "url": "/ualuck/static/js/2.c6cfa4dc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ualuck/static/js/2.c6cfa4dc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72246167e03a2d21680e",
+    "url": "/ualuck/static/js/main.ece0dd20.chunk.js"
+  },
+  {
+    "revision": "c1ead019d9aec175faa1",
+    "url": "/ualuck/static/js/runtime-main.7ebda47b.js"
+  },
+  {
+    "revision": "9d9b881300d47544d24fd4384b6abcfa",
+    "url": "/ualuck/static/media/bg.9d9b8813.jpg"
+  },
+  {
+    "revision": "758b153bcd81ded96fe620c9dc75f86e",
+    "url": "/ualuck/static/media/icons.758b153b.png"
+  },
+  {
+    "revision": "d3bcb63cf9646a19936088d49af02157",
+    "url": "/ualuck/static/media/soccer-player.d3bcb63c.png"
+  },
+  {
+    "revision": "4c7ab30d46d2a3bc50e7a6a4636a9eb8",
+    "url": "/ualuck/static/media/standard.4c7ab30d.png"
+  }
+]);
